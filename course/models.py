4498099589c0af5c2588b9515d8e351223f31a59
AntonSkyrda <mr.skyrda@gmail.com>
2025-02-12 18:00:30 +0200

    create course app and models

---
+++ course/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    teacher = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="courses"
+    )
+    groups = models.ManyToManyField("Group", related_name="courses", blank=True)
+
+    def __str__(self):
+        return self.name
